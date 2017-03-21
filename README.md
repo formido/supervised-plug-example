554bde0e5ad2146ce4bd60d35b40359dad2aa6cb
Michael Terry <michael@michaelterry.org>
2017-03-21 15:53:28 -0700

    Supervised plug

---
+++ README.md
@@ -0,0 +1,24 @@
+# PlugApp
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `plug_app` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:plug_app, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `plug_app` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:plug_app]]
+    end
+    ```
+
---
+++ lib/plug_app.ex
@@ -0,0 +1,33 @@
+defmodule PlugApp do
+  use Application
+
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    children = [
+      # Define workers and child supervisors to be supervised
+      Plug.Adapters.Cowboy.child_spec(:http, MyPlug, [], [port: 4001])
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MyApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
+
+defmodule MyPlug do
+  import Plug.Conn
+
+  def init(options) do
+    # initialize options
+
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello world")
+  end
+end
